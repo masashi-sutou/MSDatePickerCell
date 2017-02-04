a4de3c6405dd85e954f381c157168b306a06654b
masashi-sutou <sutou.masasi@gmail.com>
2017-02-04 14:50:37 +0900

    add Demo project

---
+++ MSDatePickerCell-Demo/MSDatePickerCell-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MSDatePickerCell-Demo
+//
+//  Created by 須藤 将史 on 2017/02/04.
+//  Copyright © 2017年 masashi_sutou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
